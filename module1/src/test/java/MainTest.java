c4c2a2a7f5e73eb215f0116882fbce00935eb784
Anastasiia Iakimova <nastya.iakimova@gmail.com>
2018-01-30 22:24:31 +0300

    add test plugin

--- module1/src/test/java/MainTest.java
+++ module1/src/test/java/MainTest.java
@@ -1,2 +1,10 @@
+import org.testng.annotations.Test;
+
+import static org.testng.AssertJUnit.assertTrue;
+
 public class MainTest {
+    @Test
+    public void debugStepTest(){
+        assertTrue(true);
+    }
 }
