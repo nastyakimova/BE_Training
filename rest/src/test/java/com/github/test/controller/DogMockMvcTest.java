729500e348a1da7dab9b05da0615ddfa9211adfa
Anastasiia Iakimova <nastya.iakimova@gmail.com>
2018-04-10 21:58:29 +0300

    add mockmvc test

--- rest/src/test/java/com/github/test/controller/DogMockMvcTest.java
+++ rest/src/test/java/com/github/test/controller/DogMockMvcTest.java
@@ -1,6 +1,7 @@
 package com.github.test.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
 import org.springframework.test.context.web.WebAppConfiguration;
@@ -10,10 +11,11 @@ import org.springframework.web.context.WebApplicationContext;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import static org.hamcrest.Matchers.containsInAnyOrder;
+import static org.hamcrest.Matchers.hasSize;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-@Test
 @ContextConfiguration("classpath:/application-context.xml")
 @WebAppConfiguration
 public class DogMockMvcTest extends AbstractTestNGSpringContextTests {
@@ -30,7 +32,30 @@ public class DogMockMvcTest extends AbstractTestNGSpringContextTests {
     @Test
     public void shouldGetAllDogs() throws Exception {
         mockMvc.perform(get("/dog"))
-                .andExpect(status().isOk());
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
+                .andExpect(jsonPath("$", hasSize(3)))
+                .andExpect(jsonPath("$..name", containsInAnyOrder("Jack", "Sam", "Tom")))
+                .andExpect(jsonPath("$..weight", containsInAnyOrder(20.1, 20.1, 20.1)))
+                .andExpect(jsonPath("$..height", containsInAnyOrder(15.4, 15.4, 15.4)));
+    }
+
+    @Test
+    void shouldFindDogById() throws Exception {
+
+    }
+
+    @Test
+    void shouldCreateDogTest() throws Exception {
+
+    }
+
+    @Test
+    void shouldUpdateDogTest() {
+    }
+
+    @Test
+    void shouldDeleteDogTest() {
     }
 }
 
